d2a94605df7d673c6b94982061f1eeb65b1b9aa7
Christopher Goebel <cmgoebel@iupui.edu>
2020-08-26 23:16:47 -0400

    adds example of opener and reader for csv

--- dateprice.py
+++ dateprice.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import csv
+
+with open("test-data/account.csv") as account_file:
+    d_reader = csv.DictReader(account_file)
+    headers = d_reader.fieldnames
+    for line in d_reader:
+        for header in headers:
+            print(f"{line[header]} ", end='')
+
+if __name__ == '__main__':
+    print("hello danny")
